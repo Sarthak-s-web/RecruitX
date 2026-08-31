1387fb2d6e7fc2ed62475bbf4ed406f0ecc20695
Sarthak-s-web <gargs0046@gmail.com>
2026-08-31 18:22:39 +0530

    Fix resource type in uploadResume function to use 'image' instead of 'raw'

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -10,7 +10,7 @@ const uploadResume = (buffer)=>{
     return new Promise((resolve, reject)=>{
         const stream = cloudinary.uploader.upload_stream({
             folder:"recruitx/resume",
-            resource_type:"raw"
+            resource_type:"image"
         },
         (error,result)=>{
             if(error){
